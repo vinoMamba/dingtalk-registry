9fa049f6eb99000b0a97d75c5e4ebbd7bb53fd31
vino <vino0908@outlook.com>
2025-04-30 15:47:46 +0800

    the first

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/registry/new-york/button/button";
+
 export function App() {
   return (
     <div>
       <h1>Hello World</h1>
+      <Button/>
     </div>
   )
 }
---
+++ src/registry/new-york/button/button.tsx
@@ -0,0 +1,3 @@
+export function Button() {
+  return <button className=" bg-red-300">Click me</button>;
+}
